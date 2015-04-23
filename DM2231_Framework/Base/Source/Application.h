6a12a60e39f2ab13d97126e52875b472e794b4e2
NothingMuch123 <curious_boii@hotmail.com>
2015-04-23 10:38:38 +0800

    Framework

---
+++ DM2231_Framework/Base/Source/Application.h
@@ -0,0 +1,28 @@
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include "timer.h"
+
+class Application
+{
+public:
+	static Application& GetInstance()
+	{
+		static Application app;
+		return app;
+	}
+	void Init();
+	void Run();
+	void Exit();
+	static bool IsKeyPressed(unsigned short key);
+
+private:
+	Application();
+	~Application();
+
+	//Declare a window object
+	StopWatch m_timer;
+};
+
+#endif
